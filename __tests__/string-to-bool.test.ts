a9be4e75e856b1da9d0188dc81fb1b9cf3930aa8
Tim Düsterhus <tim@bastelstu.be>
2024-11-03 20:47:50 +0100

    Sync with actions/typescript-action

--- __tests__/string-to-bool.test.ts
+++ __tests__/string-to-bool.test.ts
@@ -1,7 +1,7 @@
 import stringToBool from '../src/string-to-bool'
 import { expect, test } from '@jest/globals'
 
-test('truthy', async () => {
+test('truthy', () => {
   expect(stringToBool('on')).toBe(true)
   expect(stringToBool('true')).toBe(true)
   expect(stringToBool('yes')).toBe(true)
@@ -9,7 +9,7 @@ test('truthy', async () => {
   for (let i = 1; i < 10; i++) expect(stringToBool(i.toString())).toBe(true)
 })
 
-test('falsy', async () => {
+test('falsy', () => {
   expect(stringToBool('off')).toBe(false)
   expect(stringToBool('false')).toBe(false)
   expect(stringToBool('no')).toBe(false)
@@ -17,7 +17,7 @@ test('falsy', async () => {
   expect(stringToBool('0')).toBe(false)
 })
 
-test('invalid', async () => {
+test('invalid', () => {
   expect(() => stringToBool('invalid')).toThrow()
   expect(() => stringToBool('enabled')).toThrow()
   expect(() => stringToBool('disabled')).toThrow()
